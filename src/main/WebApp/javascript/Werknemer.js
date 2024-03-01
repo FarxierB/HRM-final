18f1b69bb616b9be5924cf0a3259ef0b37bc1951
FarxierB <dbechan@live.com>
2024-03-01 15:43:43 -0300

    V11

--- src/main/WebApp/javascript/Werknemer.js
+++ src/main/WebApp/javascript/Werknemer.js
@@ -4,9 +4,34 @@ const API_URL = "http://localhost:8083/HRM_final_war/api/werknemer"
 
 // Get all werknemers
 function getWerknemers() {
-    return fetch(`${API_URL}/list`)
+    /*return fetch(`${API_URL}/list`)
         .then(response => response.json())
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error:', error));*/
+    fetch(`${API_URL}/list`)
+        .then(response => response.json())
+        .then(alleWerknemers => {
+            alleWerknemers.reverse();
+            let alleWerknemersGridRow = '';
+
+            alleWerknemers.forEach(Werknemer => {
+                alleWerknemersGridRow +=
+                    `<tr>
+                        <td>${Werknemer.werknemerId}</td>
+                        <td>${Werknemer.voorNaam}</td>
+                        <td>${Werknemer.achterNaam}</td>
+                        <td>${Werknemer.email}</td>`;
+
+                alleWerknemersGridRow +=
+                    `<td>
+                        <button id="${Werknemer.werknemerId}" type="button" onclick="updateWerknemer(${Werknemer.werknemerId})" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#wijzigDienst">Details/Wijzigen</button>
+                        <button id="${Werknemer.werknemerId}" type="button" onclick="deleteWerknemer(${Werknemer.werknemerId})" class="btn btn-danger">Verwijderen</button>
+                    </td>
+                </tr>`;
+            });
+
+            document.getElementById("tableBody").innerHTML = alleWerknemersGridRow;
+        })
+        .catch(error => console.error('Error fetching werknemers:', error));
 }
 
 // Get werknemer by ID
